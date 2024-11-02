8b6b2fad21778c0171aaadf81a663710c0fc1b58
guilhermeadam <guilhermeaddam@gmail.com>
2024-11-02 10:26:57 -0300

    setup supabase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 28 }}>Dashboard Platform</Text>
-      <Text style={{ fontSize: 16, color: 'gray' }}>
-        Plataforma de Dashboard da Casa Civil
-      </Text>
+      <Text>Dashboard Platform</Text>
+      <Text>Plataforma de Dashboard da Casa Civil</Text>
+
       <StatusBar style="auto" />
     </View>
   );
