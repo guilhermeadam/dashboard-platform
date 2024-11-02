5b4f60661cac839b95be4cd0f8d556c021f02432
guilhermeadam <guilhermeaddam@gmail.com>
2024-11-02 10:54:39 -0300

    config supabase types

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { supabase } from '../lib/supabase';
 
 export default function HomeScreen() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      let { data, error } = await supabase.from('users').select('*');
+      if (error) {
+        Alert.alert('Erro ao carregar usuários');
+      }
+      console.log('Usuários', data?.[0].name);
+      setUser(data?.[0].name);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Dashboard Platform</Text>
       <Text>Plataforma de Dashboard da Casa Civil</Text>
-
+      <Text>Esse dado está vindo do supabase: {user}</Text>
       <StatusBar style="auto" />
     </View>
   );
